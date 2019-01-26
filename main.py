1f1999322a7d99c71ae47c0f94003c0bf41a31f7
CraigMcCarthy <craigmccarthy1994@gmail.com>
2019-01-26 01:10:31 -0500

    Added movement + basic box

--- main.py
+++ main.py
@@ -11,22 +11,44 @@ def main():
     # load and set the logo
     logo = pygame.image.load("logo32x32.png")
     pygame.display.set_icon(logo)
-    pygame.display.set_caption("minimal program")
+    pygame.display.set_caption("Big weiner game")
 
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240, 180))
+    screen = pygame.display.set_mode((500, 500))
 
     # define a variable to control the main loop
     running = True
 
+    x = 50
+    y = 425
+    width = 40
+    height = 60
+    vel = 5
+
     # main loop
     while running:
+        pygame.time.delay(100)
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT] and x > vel:
+            x -= vel
+        if keys[pygame.K_RIGHT] and x < 500 - width - vel:
+            x += vel
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < 500 - height - vel:
+            y += vel
+
+        screen.fill((0, 0, 0))
+        pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
+        pygame.display.update()
 
 
 # run the main function only if this module is executed as the main script
