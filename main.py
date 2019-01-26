d15801c931cef25fd16a4f0d6ccfdf1f10dc720e
CraigMcCarthy <craigmccarthy1994@gmail.com>
2019-01-26 01:34:11 -0500

    asduoih;

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 # import the pygame module, so you can use it
 import pygame
 
+
+def redrawGameWindow(screen, x, y, width, height, walkCount):
+    # screen.blit(bg, (0, 0))  # if we have the floor/background
+    if walkCount + 1 >= 27:
+        walkCount = 0
+
+    if left:
+        screen.blit(walkCount//3)
+    pygame.display.update()
+
 # define a main function
 
 
@@ -19,11 +29,23 @@ def main():
     # define a variable to control the main loop
     running = True
 
+    # add more to the array if you want frames
+    walkLeft = [pygame.image.load('Sprites/Player/Player_Left.png')]
+    walkRight = [pygame.image.load('Sprites/Player/Player_Right.png')]
+    walkUp = [pygame.image.load('Sprites/Player/Player_Up.png')]
+    walkDown = [pygame.image.load('Sprites/Player/Player_Down.png')]
+    #char = pygame.image.load('standing.png')
+
     x = 50
     y = 425
-    width = 40
-    height = 60
+    width = 64
+    height = 64
     vel = 5
+    left = False
+    right = False
+    up = False
+    down = False
+    walkCount = 0
 
     # main loop
     while running:
@@ -39,16 +61,20 @@ def main():
 
         if keys[pygame.K_LEFT] and x > vel:
             x -= vel
-        if keys[pygame.K_RIGHT] and x < 500 - width - vel:
+            left = True
+            right = False
+        elif keys[pygame.K_RIGHT] and x < 500 - width - vel:
             x += vel
+            left = False
+            right = True
         if keys[pygame.K_UP] and y > vel:
-            y -= vel
-        if keys[pygame.K_DOWN] and y < 500 - height - vel:
-            y += vel
+            up = True
+            down = False
+        elif keys[pygame.K_DOWN] and y < 500 - height - vel:
+            up = False
+            down = True
 
-        screen.fill((0, 0, 0))
-        pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
-        pygame.display.update()
+        redrawGameWindow(screen, x, y, width, height, walkCount)
 
 
 # run the main function only if this module is executed as the main script
